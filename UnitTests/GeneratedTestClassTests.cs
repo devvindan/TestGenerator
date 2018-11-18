dfa1b0f3dc791b3cddaf026b8ba2fc1577f06fb8
devvindan <devvindan@gmail.com>
2018-11-18 20:28:36 +0300

    added failed tests

--- UnitTests/GeneratedTestClassTests.cs
+++ UnitTests/GeneratedTestClassTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassGenerator;
 
-namespace GeneratedTestClass.Test
+namespace GeneratedTestClassTest
 {
     [TestClass]
     public class GeneratedTestClassTests
