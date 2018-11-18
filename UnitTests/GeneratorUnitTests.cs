1e339be4a47ed20ac9fac4d6fb9267d721977c06
devvindan <devvindan@gmail.com>
2018-11-18 20:07:42 +0300

    added more unit tests

--- UnitTests/GeneratorUnitTests.cs
+++ UnitTests/GeneratorUnitTests.cs
@@ -16,6 +16,9 @@ namespace UnitTests
     public class GeneratorUnitTests
     {
 
+        private string classOneName;
+        private string classTwoName;
+
         private string classOneCode;
         private string classTwoCode;
         private string sourceClassCode;
@@ -33,8 +36,11 @@ namespace UnitTests
             string sourceFilesDirectory = config.path_to_files;
             string generatedFilesDirectory = config.output_path;
 
-            string pathClassOne = Path.Combine(generatedFilesDirectory, "MyClassTests.cs");
-            string pathClassTwo = Path.Combine(generatedFilesDirectory, "GeneratedTestClassTests.cs");
+            classOneName = "MyClassTests.cs";
+            classTwoName = "GeneratedTestClassTests.cs";
+
+            string pathClassOne = Path.Combine(generatedFilesDirectory, classOneName);
+            string pathClassTwo = Path.Combine(generatedFilesDirectory, classTwoName);
 
             string souceClassPath = Path.Combine(sourceFilesDirectory, "MyTestClass.txt");
 
@@ -52,14 +58,96 @@ namespace UnitTests
 
         }
 
-        [TestMethod]
         // Test that generator produces two separate files instead of one
+        [TestMethod]
         public void TestNumberOfClasses()
         {
             Assert.AreEqual(generatedClasses.Count, 2);
         }
 
-        
+        // Test that code is produced by our generator.
+        [TestMethod]
+        public void TestProducedCode()
+        {
+            foreach (GeneratedTestClass gClass in generatedClasses)
+            {
+                if (gClass.Name == classOneName)
+                {
+                    Assert.IsTrue(gClass.Code == classOneCode);
+                } else
+                {
+                    Assert.IsTrue(gClass.Code == classTwoCode);
+                }
+            }
+        }
+
+        // Test method count in First and Second class
+        [TestMethod]
+        public void TestMethodCount()
+        {
+
+            int methodsOneCount = classOneRoot.DescendantNodes().OfType<MethodDeclarationSyntax>().Count();
+            Assert.AreEqual(methodsOneCount, 1);
+
+            int methodsTwoCount = classTwoRoot.DescendantNodes().OfType<MethodDeclarationSyntax>().Count();
+            Assert.AreEqual(methodsTwoCount, 2);
+
+        }
+
+        // Check number of class declarations inside generated classes
+        [TestMethod]
+        public void TestClassDeclarationCount()
+        {
+            int classesOneCount = classOneRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().Count();
+            Assert.AreEqual(classesOneCount, 1);
+
+            int classesTwoCount = classTwoRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().Count();
+            Assert.AreEqual(classesTwoCount, 1);
+        }
+
+        // Check that namespaces count is correct
+        [TestMethod]
+        public void TestNamespacesCount()
+        {
+            int namespacesOneCount = classOneRoot.DescendantNodes().OfType<NamespaceDeclarationSyntax>().Count();
+            Assert.AreEqual(namespacesOneCount, 1);
+
+            int namespacesTwoCount = classTwoRoot.DescendantNodes().OfType<NamespaceDeclarationSyntax>().Count();
+            Assert.AreEqual(namespacesTwoCount, 1);
+        }
+
+        // Check that class attributes are correct
+        [TestMethod]
+        public void TestClassAttributes()
+        {
+            Assert.AreEqual(1, classOneRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+                .Where((classDeclaration) => classDeclaration.AttributeLists.Any((attributeList) => attributeList.Attributes
+                .Any((attribute) => attribute.Name.ToString() == "TestClass"))).Count());
+
+            Assert.AreEqual(1, classTwoRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+                .Where((classDeclaration) => classDeclaration.AttributeLists.Any((attributeList) => attributeList.Attributes
+                .Any((attribute) => attribute.Name.ToString() == "TestClass"))).Count());
+        }
+
+        // Check that method attributes are correct
+        [TestMethod]
+        public void TestMethodAttributes()
+        {
+            IEnumerable<MethodDeclarationSyntax> methodsOne = classOneRoot.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            Assert.AreEqual(1, methodsOne.Where((methodDeclaration) => methodDeclaration.AttributeLists
+                .Any((attributeList) => attributeList.Attributes.Any((attribute) => attribute.Name.ToString() == "TestMethod")))
+                .Count());
+
+            IEnumerable<MethodDeclarationSyntax> methodsTwo = classTwoRoot.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            Assert.AreEqual(2, methodsTwo.Where((methodDeclaration) => methodDeclaration.AttributeLists
+                .Any((attributeList) => attributeList.Attributes.Any((attribute) => attribute.Name.ToString() == "TestMethod")))
+                .Count());
+
+        }
+
 
     }
+
 }
